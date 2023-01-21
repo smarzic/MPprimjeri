9ef742ecd895e0f4b428a514c3c96f912fe9e8a0
smarzic <sanjin.marzic@gmail.com>
2023-01-21 18:30:23 +0100

    Aktivnost s korištenjem akcelerometra
    + definirana ikona aplikacije
    + dodani resursi (zvukovi zvona, strelica za gumb)

---
+++ app/src/main/java/com/example/mp_primjeri/AccelerometerActivity.java
@@ -0,0 +1,102 @@
+package com.example.mp_primjeri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class AccelerometerActivity extends AppCompatActivity {
+
+    private String LOGTAG = "DANTE-LOG-accelerometer";
+    private MediaPlayer bell1, bell2;
+    private SensorManager sensorManager;
+    private SensorEventListener sensorEventListener;
+    private float prevX, prevY, prevZ;
+    private DecimalFormat df;
+    TextView txtX, txtY, txtZ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accelerometer);
+
+        // inicijalizacija
+        bell1 = MediaPlayer.create(this, R.raw.bell_123742);
+        bell2 = MediaPlayer.create(this, R.raw.church_bell_5993);
+        df = new DecimalFormat("+##0.00000;-##0.00000");
+        df.setMinimumFractionDigits(5);
+        df.setMaximumFractionDigits(5);
+        txtX = findViewById(R.id.txtX);
+        txtY = findViewById(R.id.txtY);
+        txtZ = findViewById(R.id.txtZ);
+
+        // gumb za povratak
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                //
+                sensorManager.unregisterListener(sensorEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
+                finish();
+            }
+        });
+
+        // akcelerometar
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                // čitanje novih očitanja
+                float x = sensorEvent.values[0];
+                float y = sensorEvent.values[1];
+                float z = sensorEvent.values[2];
+
+                // ispis očitanja u log + formatiranje da budu lakše čitljivi
+                Log.i(LOGTAG, "onSensorChanged " + df.format(x) + ", " + df.format(y) + ", " + df.format(z));
+
+                // obrada očitanja
+                if(Math.abs(x - prevX) > 20) {
+                    Log.i(LOGTAG, "small bell");
+                    bell1.start();
+                }
+                if(Math.abs(z - prevZ) > 10) {
+                    Log.i(LOGTAG, "big bell");
+                    bell2.start();
+                }
+
+                // prikaz očitanja na ekranu
+                txtX.setText(df.format(x));
+                txtY.setText(df.format(y));
+                txtZ.setText(df.format(z));
+
+                // spremanje očitanja
+                prevX = x;
+                prevY = y;
+                prevZ = z;
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+
+        sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/mp_primjeri/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.mp_primjeri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnAkcelerometar = findViewById(R.id.btnAkcelerometar);
+        btnAkcelerometar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), AccelerometerActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
